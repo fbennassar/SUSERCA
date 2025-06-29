38619f3e782c0f0d49f50ea2064a2767740ed1c8
Semeco <semecojosue18@gmail.com>
2025-06-29 17:26:39 -0400

    feat: enhance layout and functionality across multiple views, add new utility classes, and improve sidebar behavior

--- app-electron/src/main.js
+++ app-electron/src/main.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-const { app, BrowserWindow } = require('electron/main')
+require("dotenv").config();
+const { app, BrowserWindow } = require("electron/main");
 
-require('electron-reload')(process.cwd(), {
-  electron: require(`${process.cwd()}/node_modules/electron`)
+require("electron-reload")(process.cwd(), {
+  electron: require(`${process.cwd()}/node_modules/electron`),
 });
 
-require('./backend/ipc/IPCusuarios.js')
-require('./backend/ipc/IPCrol.js');
-require('./backend/ipc/IPCclientes.js');
-require('./backend/ipc/IPCproveedores');
+require("./backend/ipc/IPCusuarios.js");
+require("./backend/ipc/IPCrol.js");
+require("./backend/ipc/IPCclientes.js");
+require("./backend/ipc/IPCproveedores");
 
-const db = require('./backend/db/supabaseClient.js');
-const path = require('path');
+const db = require("./backend/db/supabaseClient.js");
+const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -23,32 +23,44 @@ const createWindow = () => {
     minHeight: 730,
     maxWidth: 1920,
     maxHeight: 1080,
-    icon: path.join(__dirname, '../assets/icons/general/icon.ico'), // Usa .ico para Windows
+    icon: path.join(__dirname, "../assets/icons/general/icon.ico"), // Usa .ico para Windows
     webPreferences: {
-      preload: path.join(process.cwd(), './app-electron/src/preload.js')
-    }
-  })
+      preload: path.join(process.cwd(), "./app-electron/src/preload.js"),
+    },
+  });
 
-  win.loadFile('./app-electron/src/views/login.html')
-}
+  win.loadFile("./app-electron/src/views/login.html");
+};
 
-const { ipcMain } = require('electron/main');
-ipcMain.on('close-app', () => {
+const { ipcMain } = require("electron/main");
+ipcMain.on("close-app", () => {
   app.quit();
 });
 
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+ipcMain.handle("get-profile-seguro", async () => {
+  try {
+    // Ajusta la ruta si tu session.js está en otro lugar
+    const session = require("../src/backend/session/session.js");
+    const profile = await session.getProfile();
+    return profile;
+  } catch (error) {
+    console.error("Error al obtener el perfil seguro:", error);
+    return null;
   }
-})
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
